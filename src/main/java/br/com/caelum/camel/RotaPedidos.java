213c2f771b7840d8d4e6fc18ed740711c6e277cb
ajoluam <ajoluam@hotmail.com>
2019-10-10 21:51:09 -0300

    Aula 2 - split, filter e xpath

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -9,27 +9,35 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-		//no meu contexto , adiciono uma nova rota que será definida no configue logo abaixo
+		// no meu contexto , adiciono uma nova rota que será definida no configue logo
+		// abaixo
 		context.addRoutes(new RouteBuilder() {
-			
-			
+
 			@Override
 			public void configure() throws Exception {
-				//para primeiro exemplo vamos pegar os arquivos que estão na pasta pedidos
-				//e transferi-los para a pasta saida
-				//o parâmetro noop=true não apaga os arquivos da pasta origem
+				// para primeiro exemplo vamos pegar os arquivos que estão na pasta pedidos
+				// nos métodos From e To estão os endpoints para o trafego dos arquivos
+				// e transferi-los para a pasta saida
+				// o parâmetro noop=true não apaga os arquivos da pasta origem
 				from("file:pedidos?delay=5s&noop=true").
-					log("${id}").marshal().xmljson().
+					split().
+						xpath("/pedido/itens/item").
+					filter().
+						xpath("/item/formato[text()='EBOOK']").
+					log("${exchange.pattern}"). // essa expressão de linguagem mostra o Exchange Pattern utilizado, como é uma via unidirecional aparecerá InOnly
+					log("${id}").
+					marshal().// indica que queremos transformar a mensagem de um formato para outro
+						xmljson().
 					log("${body}").
 					setHeader("CamelFileName", simple("${file:name.noext}.json")).
 				to("file:saida");
-				
+
 			}
 		});
-		
+
 		context.start();
 		Thread.sleep(20000);
 		context.stop();
 
-	}	
+	}
 }
