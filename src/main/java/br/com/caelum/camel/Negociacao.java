d6c780985a8702c1537de8a4aa7c680f32816f85
alexandre.j.amorim <alexandre.j.amorim@CPX-IBCIWHBRP5O.dir.svc.accenture.com>
2019-10-15 17:55:24 -0300

    Aula 3 final

---
+++ src/main/java/br/com/caelum/camel/Negociacao.java
@@ -0,0 +1,46 @@
+package br.com.caelum.camel;
+
+import java.util.Calendar;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import java.sql.Date;
+
+
+public class Negociacao {
+	
+	private double preco;
+	private int quantidade;
+	private Calendar data;
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	@Override
+	public String toString() {
+		return "Negociacao [preco=" + preco + ", quantidade=" + quantidade + "]";
+	}
+	
+	
+	
+		
+		
+
+}
--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,18 +1,39 @@
 package br.com.caelum.camel;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+
+
+
+import com.thoughtworks.xstream.XStream;
 
 public class RotaPedidos {
 
+	private static com.mysql.cj.jdbc.MysqlConnectionPoolDataSource criaDataSource() {
+		com.mysql.cj.jdbc.MysqlConnectionPoolDataSource mysqlDs = new com.mysql.cj.jdbc.MysqlConnectionPoolDataSource();
+		mysqlDs.setDatabaseName("camel");
+		mysqlDs.setServerName("localhost");
+		mysqlDs.setPort(3306);
+		mysqlDs.setUser("root");
+		mysqlDs.setPassword("Mozao123");
+		return mysqlDs;
+	}
+	
 	public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
-		// no meu contexto , adiciono uma nova rota que será definida no configue logo
-		// abaixo
+		//CamelContext context = new DefaultCamelContext();
+		
+		SimpleRegistry registro = new SimpleRegistry();
+		registro.put("mysql", criaDataSource());
+		CamelContext context = new DefaultCamelContext(registro);//construtor recebe registro
 		
 		RouteBuilder rota1 = new RouteBuilder() {
 			
@@ -45,18 +66,41 @@ public class RotaPedidos {
 			
 		};
 
+		
+		
+				
 		RouteBuilder rota2 = new RouteBuilder() {
 			
 			@Override
 			public void configure() throws Exception {
 				
+				final XStream xstream = new XStream();
+				xstream.alias("negociacao", Negociacao.class);
+				
 				from("timer://negociacoes?fixedRate=true&delay=1s&period=360s").
 					to("http4://argentumws-spring.herokuapp.com/negociacoes").
-					split().xpath("/list/negociacao").
 					convertBodyTo(String.class).
-					log("${body}").
-					setHeader(Exchange.FILE_NAME, simple("negociacao-${header.CamelSplitIndex}.xml")).
-				to("file:saida");
+						unmarshal(new XStreamDataFormat(xstream)).
+					split(body()).
+					process(new Processor() {
+						
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							Negociacao negociacao = exchange.getIn().getBody(Negociacao.class);
+				            exchange.setProperty("preco", negociacao.getPreco());
+				            exchange.setProperty("quantidade", negociacao.getQuantidade());
+				            String data = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(negociacao.getData().getTime());
+				            exchange.setProperty("data", data);
+							
+						}
+					}).
+					setBody(simple("insert into negociacao (preco,quantidade,dataDoNegocio) values ('${property.preco}','${property.quantidade}','${property.data}')")).
+					log("${body}"). //logando o comando sql
+					delay(1000).
+				to("jdbc:mysql");
+					//end();
+					//setHeader(Exchange.FILE_NAME, simple("negociacao-${header.CamelSplitIndex}.xml")).
+				//to("file:saida");
 
 			}
 		
